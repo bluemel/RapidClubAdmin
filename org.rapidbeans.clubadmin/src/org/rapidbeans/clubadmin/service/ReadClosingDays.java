64cd88a5ade5199447254603b66208fba4884a99
bluemel <martin.bluemel@web.de>
2013-12-30 23:43:23 +0100

    Read closing days. Further development

--- org.rapidbeans.clubadmin/src/org/rapidbeans/clubadmin/service/ReadClosingDays.java
+++ org.rapidbeans.clubadmin/src/org/rapidbeans/clubadmin/service/ReadClosingDays.java
@@ -8,12 +8,16 @@
 
 package org.rapidbeans.clubadmin.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.rapidbeans.clubadmin.closingdays.SchulferienReader;
 import org.rapidbeans.clubadmin.domain.ClosingPeriod;
+import org.rapidbeans.clubadmin.domain.Location;
 import org.rapidbeans.clubadmin.domain.MasterData;
 import org.rapidbeans.clubadmin.presentation.RapidClubAdminClient;
+import org.rapidbeans.core.basic.PropertyDate;
+import org.rapidbeans.core.common.PrecisionDate;
 import org.rapidbeans.presentation.ApplicationManager;
 import org.rapidbeans.service.Action;
 
@@ -25,17 +29,33 @@ import org.rapidbeans.service.Action;
 public class ReadClosingDays extends Action {
 
     /**
-     * open the training editing view.
+     * Import new closing periods of the year 2014. TODO - Jahr einstellen
+     * lassen - Containments von Feiertagen ausschließen - GUI-Texte
      */
     public void execute() {
         final RapidClubAdminClient app = (RapidClubAdminClient) ApplicationManager.getApplication();
         final MasterData masterdata = app.getMasterData();
-        final List<ClosingPeriod> newClosingPeriods = new SchulferienReader().readSchulferienAndFeiertage(
+        final List<ClosingPeriod> foundClosingPeriods = new SchulferienReader().readSchulferienAndFeiertage(
                 "www.schulferien.org", "bayern", "2014");
-        for (final ClosingPeriod cp : newClosingPeriods) {
+        final StringBuilder cpList = new StringBuilder();
+        final List<ClosingPeriod> newClosingPeriods = new ArrayList<ClosingPeriod>();
+        for (final ClosingPeriod cp : foundClosingPeriods) {
             if (!masterdata.getClosingperiods().contains(cp)) {
-                masterdata.addClosingperiod(cp);
+                newClosingPeriods.add(cp);
             }
         }
+        if (newClosingPeriods.size() == 0) {
+            cpList.append("Es wurden keine neuen Schlie" + Umlaut.SUML + "zeitr" + Umlaut.L_AUML + "me angelegt!");
+        } else {
+            cpList.append("Folgende Schlie" + Umlaut.SUML + "zeitr" + Umlaut.L_AUML + "me wurden neu angelegt:\n");
+            for (final ClosingPeriod cp : newClosingPeriods) {
+                masterdata.addClosingperiod(cp);
+                for (final Location loc : masterdata.getLocations()) {
+                    cp.addLocation(loc);
+                }
+                cpList.append("- " + cp.getName() + " " + PropertyDate.format(cp.getFrom(), PrecisionDate.year) + "\n");
+            }
+        }
+        app.messageInfo(cpList.toString());
     }
 }
